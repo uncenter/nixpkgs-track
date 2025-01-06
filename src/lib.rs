41069f7cb17636e5ad9c0592d8960a4bb3678405
Orual <orual.nbc@proton.me>
2025-01-06 12:45:34 -0500

    perf: improve async/concurrency handling (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ use serde::Deserialize;
 
 const BASE_API_URL: &str = "https://api.github.com/repos/nixos/nixpkgs";
 
-fn build_request(url: &str, token: Option<&str>) -> RequestBuilder {
-	let mut request = Client::new()
+fn build_request(client: impl AsRef<reqwest::Client>, url: &str, token: Option<&str>) -> RequestBuilder {
+	let mut request = client.as_ref()
 		.get(url)
 		.header("User-Agent", "nixpkgs-track");
 
@@ -19,9 +19,9 @@ fn build_request(url: &str, token: Option<&str>) -> RequestBuilder {
 	request
 }
 
-pub async fn fetch_nixpkgs_pull_request(pull_request: u64, token: Option<&str>) -> Result<PullRequest> {
+pub async fn fetch_nixpkgs_pull_request(client:  impl AsRef<reqwest::Client>, pull_request: u64, token: Option<&str>) -> Result<PullRequest> {
 	let url = format!("{}/pulls/{}", BASE_API_URL, pull_request);
-	let response = build_request(&url, token)
+	let response = build_request(client, &url, token)
 		.send()
 		.await?
 		.error_for_status()?
@@ -31,10 +31,10 @@ pub async fn fetch_nixpkgs_pull_request(pull_request: u64, token: Option<&str>) 
 	Ok(response)
 }
 
-pub async fn branch_contains_commit(branch: &str, commit: &str, token: Option<&str>) -> Result<bool> {
+pub async fn branch_contains_commit(client:  impl AsRef<reqwest::Client>, branch: &str, commit: &str, token: Option<&str>) -> Result<bool> {
 	let url = format!("{}/compare/{}...{}", BASE_API_URL, branch, commit);
 
-	let response = build_request(&url, token)
+	let response = build_request(client,&url, token)
 		.send()
 		.await?;
 	Ok(match response.status() {
