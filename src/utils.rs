4197bca0c6320b691cda460614a655d59bc833c3
uncenter <47499684+uncenter@users.noreply.github.com>
2025-01-07 09:45:03 -0500

    refactor: apply clippy fixes, cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,12 @@
-pub fn format_seconds_to_time_ago(seconds: u64) -> String {
-	fn format_unit(value: u64, unit: &str) -> Option<String> {
-		if value > 0 {
-			Some(format!("{} {}{}", value, unit, if value > 1 { "s" } else { "" }))
-		} else {
-			None
-		}
+fn format_unit(value: u64, unit: &str) -> Option<String> {
+	if value > 0 {
+		Some(format!("{} {}{}", value, unit, if value > 1 { "s" } else { "" }))
+	} else {
+		None
 	}
+}
 
+pub fn format_seconds_to_time_ago(seconds: u64) -> String {
 	let minutes = seconds / 60;
 	let hours = minutes / 60;
 	let days = hours / 24;
@@ -15,22 +15,19 @@ pub fn format_seconds_to_time_ago(seconds: u64) -> String {
 	let remaining_minutes = minutes % 60;
 	let remaining_hours = hours % 24;
 
-	let mut result: Vec<String> = [
+	let result: Vec<String> = [
 		format_unit(days, "day"),
 		format_unit(remaining_hours, "hour"),
 		format_unit(remaining_minutes, "minute"),
 		format_unit(remaining_seconds, "second"),
 	]
-	.iter()
-	.filter_map(|x| x.clone())
+	.into_iter()
+	.flatten()
 	.collect();
 
-	if result.is_empty() {
-		"0 seconds".to_string()
-	} else if result.len() == 1 {
-		result[0].clone()
-	} else {
-		let last = result.pop().unwrap();
-		format!("{} and {}", result.join(" "), last)
+	match result.as_slice() {
+		[] => "0 seconds".to_string(),
+		[single] => single.clone(),
+		[.., last] => format!("{} and {}", result[..result.len() - 1].join(" "), last),
 	}
 }
